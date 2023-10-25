783502f24609be1d943b8f92f882ee298522cc3c
GVMartin23 <MartinGV19@gcc.edu>
2023-10-25 14:19:38 -0400

    gitignore, stuff needed for login hashing if we want that

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_required
+from flask_login import login_user, logout_user, current_user
 import os, sys
+
+from hashing_examples import UpdatedHasher
 
 script_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(script_dir)
 
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 dbpath = os.path.join(scriptdir, 'WebsiteCreator.sqlite3')
+pepfile = os.path.join(script_dir, "pepper.bin")
+
+with open(pepfile, 'rb') as fin:
+    pepper_key = fin.read()
+
+pwd_hasher = UpdatedHasher(pepper_key)
 
 # Configure the Flask App
 app = Flask(__name__)
