a64634c245aea289c63dd976969159e2802cc816
GVMartin23 <MartinGV19@gcc.edu>
2023-11-12 12:31:52 -0500

    Foreign Keys

--- db_setup.py
+++ db_setup.py
@@ -16,25 +16,30 @@ def setup_web_builder_tables(
         college = db.Column(db.Unicode, nullable=True)  # maybe enum?
         about = db.Column(db.Unicode, nullable=True)
         github = db.Column(db.Unicode, nullable=True)
+        projects = db.relationship("Project", backref='user')
+        clubs = db.relationship("Club", backref='user')
+        experiences = db.relationship("Experience", backref="user")
+        website = db.relationship("Website", backref='user')
 
     class Project(db.Model):
         __tablename__ = "Projects"
         id = db.Column(db.Integer, primary_key=True)
-        userId = db.Column(db.Integer)  # TODO: FK User
+        userId = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
         title = db.Column(db.Unicode, nullable=False)
         description = db.Column(db.Unicode, nullable=False)
         repositoryLink = db.Column(db.Unicode, nullable=True)
+        images = db.relationship('Poject_Image', backref='project')
 
     class Project_Image(db.Model):
         __tablename__ = "Project_Images"
         id = db.Column(db.Integer, primary_key=True)
-        projectId = db.Column(db.Integer)  # TODO: FK Project
+        projectId = db.Column(db.Integer, db.ForeignKey('Projects.id'), nullable=False)
         imageLink = db.Column(db.Unicode, nullable=False)
 
     class Club(db.Model):
         __tablename__ = "Clubs"
         id = db.Column(db.Integer, primary_key=True)
-        userId = db.Column(db.Integer)  # TODO: FK User
+        userId = db.Column(db.Integer, db.ForeginKey('Users.id'), nullable=False)
         title = db.Column(db.Unicode, nullable=False)
         description = db.Column(db.Unicode, nullable=False)
         position = db.Column(db.Unicode, nullable=True)
@@ -42,7 +47,7 @@ def setup_web_builder_tables(
     class Experience(db.Model):
         __tablename__ = "Experiences"
         id = db.Column(db.Integer, primary_key=True)
-        userId = db.Column(db.Integer)  # TODO: FK User
+        userId = db.Column(db.Integer, db.ForeginKey('Users.id'), nullable=False)
         company = db.Column(db.Unicode, nullable=False)
         description = db.Column(db.Unicode, nullable=False)
         position = db.Column(db.Unicode, nullable=False)
@@ -51,7 +56,7 @@ def setup_web_builder_tables(
     class Website(db.Model):
         __tablename__ = "Websites"
         id = db.Column(db.Integer, primary_key=True)
-        userId = db.Column(db.Integer)  # TODO: FK User
+        userId = db.Column(db.Integer, db.ForeginKey('Users.id'), nullable=False)
         websiteLink = db.Column(db.Unicode, unique=True)
         homePage = db.Column(db.Unicode, nullable=False)
         aboutPage = db.Column(db.Unicode, nullable=True)
